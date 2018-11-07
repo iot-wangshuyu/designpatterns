a85655c5c9cf4407a202ccd04b636b7073c4d615
shuyu.wang <wangsy@ganinfo.com>
2018-11-07 13:10:36 +0800

    新建简单工厂模式工程

---
+++ simplefactory/src/main/java/com/shuyu/SimplefactoryApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplefactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplefactoryApplication.class, args);
+	}
+}
