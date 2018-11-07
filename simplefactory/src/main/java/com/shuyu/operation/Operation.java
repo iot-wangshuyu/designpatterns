82ad24c51c91fcd8f0d5d82a60f7401c0819137e
shuyu.wang <wangsy@ganinfo.com>
2018-11-07 13:31:51 +0800

    实现运算基类

---
+++ simplefactory/src/main/java/com/shuyu/operation/Operation.java
@@ -0,0 +1,22 @@
+package com.shuyu.operation;
+
+import lombok.Data;
+
+/**
+ * @author Shuyu.Wang
+ * @package:com.shuyu.operation
+ * @className:
+ * @description:基本运算类
+ * @date 2018-11-07 13:11
+ **/
+@Data
+public class Operation {
+    private double numberA=0;
+    private double numberB=0;
+
+    public  double getResult(){
+        double result=0;
+        return result;
+    }
+
+}
