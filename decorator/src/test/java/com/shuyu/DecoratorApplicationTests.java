113475e2dad1192d53325f9e1882e25779713287
shuyu.wang <wangsy@ganinfo.com>
2018-11-09 22:51:15 +0800

    测试类

--- decorator/src/test/java/com/shuyu/DecoratorApplicationTests.java
+++ decorator/src/test/java/com/shuyu/DecoratorApplicationTests.java
@@ -1,5 +1,7 @@
 package com.shuyu;
 
+import com.shuyu.decorator.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +9,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class DecoratorApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Person person=new Person("小菜");
+		log.info("第一种打扮");
+		Sneakers sneakers=new Sneakers();
+		BigTrouser bigTrouser=new BigTrouser();
+		TShirts tShirts=new TShirts();
+		sneakers.Decorate(person);
+		bigTrouser.Decorate(sneakers);
+		tShirts.Decorate(bigTrouser);
+		tShirts.Show();
+
+		log.info("第二种打扮");
+		LeatherShoes leatherShoes=new LeatherShoes();
+		TShirts tShirts1=new TShirts();
+		leatherShoes.Decorate(person);
+		tShirts1.Decorate(leatherShoes);
+		tShirts1.Show();
 	}
 
 }
