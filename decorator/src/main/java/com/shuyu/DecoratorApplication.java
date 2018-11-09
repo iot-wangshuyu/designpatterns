16aff22977467b9a2309e508f6af8b55407514de
shuyu.wang <wangsy@ganinfo.com>
2018-11-09 17:55:54 +0800

    创建装饰模式项目

---
+++ decorator/src/main/java/com/shuyu/DecoratorApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoratorApplication.class, args);
+	}
+}
