69349b0b758260e385c15a39a197844ae1906b28
shuyu.wang <wangsy@ganinfo.com>
2018-11-09 09:43:20 +0800

    修改Context

--- strategy/src/main/java/com/shuyu/strategy/Context.java
+++ strategy/src/main/java/com/shuyu/strategy/Context.java
@@ -3,25 +3,15 @@ package com.shuyu.strategy;
 /**
  * @author Shuyu.Wang
  * @package:com.shuyu.strategy
- * @description:策略切换结合简单工厂模式实现
+ * @description:
  * @date 2018-11-08 17:10
  **/
 
 public class Context {
     Strategy strategy = null;
 
-    public Context(String type) {
-        switch (type) {
-            case "add":
-                strategy = new OperationAdd();
-                break;
-            case "sub":
-                strategy = new OperationSub();
-                break;
-            case "mul":
-                strategy = new OperationSub();
-                break;
-        }
+    public Context(Strategy strategy ) {
+        this.strategy=strategy;
     }
 
     public double operation(double a,double b){
--- strategy/src/test/java/com/shuyu/StrategyApplicationTests.java
+++ strategy/src/test/java/com/shuyu/StrategyApplicationTests.java
@@ -1,6 +1,8 @@
 package com.shuyu;
 
 import com.shuyu.strategy.Context;
+import com.shuyu.strategy.OperationAdd;
+import com.shuyu.strategy.OperationSub;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +14,9 @@ public class StrategyApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		Context context=new Context("add");
-
+		Context context=new Context(new OperationAdd());
+		System.out.println(context.operation(10,5));
+		context=new Context(new OperationSub());
 		System.out.println(context.operation(10,5));
 	}
 
