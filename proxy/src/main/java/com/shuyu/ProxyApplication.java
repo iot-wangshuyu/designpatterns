ea83df8b05c7a1a1603ac9bcddf132c8a6f5328a
shuyu.wang <wangsy@ganinfo.com>
2018-11-12 22:17:15 +0800

    代理模式工程创建

---
+++ proxy/src/main/java/com/shuyu/ProxyApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+}
