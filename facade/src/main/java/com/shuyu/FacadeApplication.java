342d2d8442d3690a4c1444832ef6fce8b88e932b
shuyu.wang <wangsy@ganinfo.com>
2018-11-18 22:42:30 +0800

    外观模式工程创建

---
+++ facade/src/main/java/com/shuyu/FacadeApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacadeApplication.class, args);
+	}
+}
