07f26c40d909789b231cd3af313aea4d53421c75
shuyu.wang <wangsy@ganinfo.com>
2018-11-20 22:54:54 +0800

    观察者模式工程创建

---
+++ publish/src/main/java/com/shuyu/PublishApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishApplication.class, args);
+	}
+}
