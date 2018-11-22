8335c40c8587aabad609efeb3c20c912162cfaf0
shuyu.wang <wangsy@ganinfo.com>
2018-11-22 21:39:29 +0800

    单列工程创建

---
+++ singleton/src/main/java/com/shuyu/SingletonApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingletonApplication.class, args);
+	}
+}
