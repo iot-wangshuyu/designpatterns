6aa984054b49ebf1a84b95cd81b080d8f5d9b0a8
shuyu.wang <wangsy@ganinfo.com>
2018-11-25 21:18:32 +0800

    状态模式工程创建

---
+++ state/src/main/java/com/shuyu/StateApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateApplication.class, args);
+	}
+}
