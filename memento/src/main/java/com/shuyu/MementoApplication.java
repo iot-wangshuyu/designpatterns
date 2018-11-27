83319395e72f8ff5a996175984a89528632880ce
shuyu.wang <wangsy@ganinfo.com>
2018-11-27 21:27:31 +0800

    备忘录模式创建

---
+++ memento/src/main/java/com/shuyu/MementoApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementoApplication.class, args);
+	}
+}
