f3427f4d2f17a6896ead7f35bffd5be1c91b0a07
shuyu.wang <wangsy@ganinfo.com>
2018-11-28 13:26:33 +0800

    组合模式工程构建

---
+++ composite/src/main/java/com/shuyu/CompositeApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeApplication.class, args);
+	}
+}
