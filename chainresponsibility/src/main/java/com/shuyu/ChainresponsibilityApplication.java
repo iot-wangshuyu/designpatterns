43ed9fc582f7581e663862a9448ec020806ec71c
shuyu.wang <wangsy@ganinfo.com>
2018-12-04 23:35:23 +0800

    职责链模式工程创建

---
+++ chainresponsibility/src/main/java/com/shuyu/ChainresponsibilityApplication.java
@@ -0,0 +1,12 @@
+package com.shuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainresponsibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainresponsibilityApplication.class, args);
+	}
+}
